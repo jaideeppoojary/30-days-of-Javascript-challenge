b901340a4bad4559af3535b08af95758ddafa378
Jaideep <jaideep.jaidee@gmail.com>
2024-07-13 12:44:50 +0530

    Add achievement notes to day1.js

--- day1.js
+++ day1.js
@@ -82,5 +82,9 @@ console.log("[>] feature2Const : ", feature2Const);
 // feature2Const = '50%'; // Error: Assignment to constant variable.
 console.log('Error: Assignment to constant variable.');
 
-
+// Achievement:
+// * Know how to declare variables using `var`, `let`, and `const`.
+// * Understand the different data types in JavaScript.
+// * Be able to use the `typeof` operator to identify the data type of a variable.
+// * Understand the concept of variable reassignment and the immutability of `const` variables.
 
